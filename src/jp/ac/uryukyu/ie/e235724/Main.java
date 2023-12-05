fe5fe96dffa68a4171d4795d25b56f0b81adfaec
Marina Sakugawa <e235724@ie.u-ryukyu.ac.jp>
2023-12-05 16:01:06 +0900

    3rd commit

--- src/jp/ac/uryukyu/ie/e235724/Main.java
+++ src/jp/ac/uryukyu/ie/e235724/Main.java
@@ -2,13 +2,13 @@ package jp.ac.uryukyu.ie.e235724;
 
 public class Main {
     public static void main(String[] args){
-        try{
-            String str = null;
-            System.out.println(str.length());
-        }catch(NullPointerException n){
-            System.out.println("Error : NullPointerException");
-            System.out.println(n.getMessage());
-        }
+        //条件1: String型の変数strに、文字列"壱百満"を代入せよ。
+        String str = "壱百満";
+
+        //条件2: Integer.parseInt(String s)メソッドを用いて、int型変数valueに代入するコードを記述せよ。
+        int value = Integer.parseInt(str);
+
+        System.out.println(value);
 
     }
     
